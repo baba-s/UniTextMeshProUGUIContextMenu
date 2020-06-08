30f911e975a402cc4e2b7c6b4e0cea18e948833d
baba_s <shotababa1206@gmail.com>
2020-06-08 19:37:41 +0900

    first commit

---
+++ Editor/TextMeshProUGUIContextMenu.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+using UnityEditor;
+using UnityEditor.Presets;
+using UnityEngine.UI;
+
+namespace Kogane.Internal
+{
+	internal static class TextMeshProUGUIContextMenu
+	{
+		private const string MENU_ITEM_NAME = "CONTEXT/TextMeshProUGUI/Add ContentSizeFitter";
+
+		[MenuItem( MENU_ITEM_NAME )]
+		private static void AddContentSizeFitter( MenuCommand command )
+		{
+			var textMeshProUGUI   = ( TextMeshProUGUI ) command.context;
+			var gameObject        = textMeshProUGUI.gameObject;
+			var contentSizeFitter = Undo.AddComponent<ContentSizeFitter>( gameObject );
+			var presets           = Preset.GetDefaultPresetsForObject( contentSizeFitter );
+
+			if ( presets == null || presets.Length <= 0 ) return;
+
+			var preset = presets[ 0 ];
+
+			preset.ApplyTo( contentSizeFitter );
+		}
+
+		[MenuItem( MENU_ITEM_NAME, true )]
+		private static bool CanAddContentSizeFitter( MenuCommand command )
+		{
+			var textMeshProUGUI = ( TextMeshProUGUI ) command.context;
+			var contentSizeFitter = textMeshProUGUI.GetComponent<ContentSizeFitter>();
+
+			return contentSizeFitter == null;
+		}
+	}
+}
